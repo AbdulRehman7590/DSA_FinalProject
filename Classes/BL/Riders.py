7998f74a5a9cd30cd33358ce67fac87c2d8db5ae
AbdulRehman7590 <120157338+AbdulRehman7590@users.noreply.github.com>
2023-12-05 23:17:36 +0500

    Managing classes and images

--- Classes/BL/Riders.py
+++ Classes/BL/Riders.py
@@ -1,4 +1,31 @@
-from Users import User
-class Riders(User):
-        def __init__(self,username,email,password,address):
+# ------------------------ Libraries ------------------------------- #
+from classes.BL.users import User
+
+# ----------------------- Riders CLass ----------------------------- #
+class Rider(User):
+        def __init__(self,username,email,password,address,rider_id):
                 super().__init__(username, email, password, address)
+                
+                self._rider_id = rider_id
+                self._is_available = True
+                self._order_list = None
+
+
+        # ------------------------ Methods ------------------------------ #
+        def view_all_orders(self):
+                pass
+
+        def view_all_delivered_orders(self):
+                pass
+
+        def view_all_pending_orders(self):
+                pass
+
+        def view_all_orders_by_address(self):
+                pass
+
+        def view_order(self,order_id):
+                pass
+
+        def view_profile(self):
+                return super().view_profile()
--- Classes/BL/Users.py
+++ Classes/BL/Users.py
@@ -1,41 +1,51 @@
+# ------------------------ Libraries ------------------------------- #
 import typing
 
+# ------------------------ User CLass ------------------------------ #
 class User:
     def __init__(self,username:str ,email: str ,password: str,address: str ) -> None:
         self.__username = username
         self.__email = email
         self.__password = password
         self.__address = address
-################################# Getter ##############################
+    
+    # ------------------------ Getter ------------------------------ #
     @property
     def username(self)->str:
         return self.__username
+    
     @property
     def email(self)->str:
         return self.__email
+    
     @property
     def password(self)->str:
         return self.__password
+    
     @property
     def address(self)->str:
         return self.__address
-################################ Setter ################################
-
-
-
+    
+    # ------------------------ Setter ------------------------------ #
     @username.setter 
     def username(self, username: str)->None:
         self.__username = username
+
     @email.setter    
     def email(self, email :str) ->None:
         self.__email = email
+
     @password.setter    
     def password(self, password :str) ->None:
         self.__password = password
+
     @address.setter    
     def address(self, address : str)->None:
         self.__address = address
 
 
+    # ------------------------ Methods ------------------------------ #
+    def view_profile(self)->str:
+        return f"Username: {self.__username}\nEmail: {self.__email}\nAddress: {self.__address}"
 
 
