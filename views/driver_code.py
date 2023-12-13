8af0338a8a0cc9de666f5be7b4d0f7bff1ebd454
amnanadeem166 <amnanadeem166@gmail.com>
2023-12-13 08:41:46 +0500

    add food gui

--- views/driver_code.py
+++ views/driver_code.py
@@ -11,7 +11,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 from utils.sign_In_Up import *
-from classes.DL.usersDL import UsersDL as dL
+from Classes.DL.usersDL import UsersDL as dL
 
 # ---------------------- Program -------------------------------- #
 class Mainwindow(QMainWindow):
