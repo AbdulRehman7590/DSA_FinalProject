28bb7b06e4c596b58e7a22449af00f20ea8255cc
AbdulRehman7590 <120157338+AbdulRehman7590@users.noreply.github.com>
2023-12-13 18:23:51 +0500

    integrating almost all buttons

--- views/driver_code.py
+++ views/driver_code.py
@@ -11,7 +11,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 from utils.sign_In_Up import *
-from Classes.DL.usersDL import UsersDL as dL
+from classes.DL.usersDL import UsersDL as dL
 
 # ---------------------- Program -------------------------------- #
 class Mainwindow(QMainWindow):
@@ -27,11 +27,10 @@ class Mainwindow(QMainWindow):
 
         # ---------------------- Defaults ------------------------ #
         self.mainStack = self.findChild(QStackedWidget, "main_Stack")
-        self.adminStack = self.findChild(QStackedWidget,"adminoption_pages")
+        self.adminStack = self.findChild(QStackedWidget,"adminoption_stack")
+        self.adminoption_stack = self.findChild(QStackedWidget,"admin_suboptions_stack")
         self.customerStack = self.findChild(QStackedWidget,"customerpages_stack")
         self.changing_mainStack_PageNo(0)
-        self.changing_adminStack_PageNo(0)
-        self.changing_customerStack_PageNo(0)
 
 
         # ------------------- Button Connections ------------------ #
@@ -42,16 +41,41 @@ class Mainwindow(QMainWindow):
         self.about_Btn_2.clicked.connect(lambda: self.changing_mainStack_PageNo(2))
         self.register_Btn.clicked.connect(lambda: TakeInput_SignIn(self))
         self.login_Btn.clicked.connect(lambda: TakeInput_LogIn(self))
-        self.logout_Btn.clicked.connect(lambda: self.changing_mainStack_PageNo(0))
-        self.logout_Btn_2.clicked.connect(lambda: self.changing_mainStack_PageNo(0))
+        self.admin_logout_Btn.clicked.connect(lambda: self.changing_mainStack_PageNo(0))
+        self.customer_logout_Btn.clicked.connect(lambda: self.changing_mainStack_PageNo(0))
+
+
+        # ---------------------- Admin Options ------------------------ #
+        self.viewFood_Btn.clicked.connect(lambda: self.changing_adminoption_stack_PageNo(0))
+        self.viewOrders_Btn.clicked.connect(lambda: self.changing_adminoption_stack_PageNo(1))
+        self.viewCustomers_Btn.clicked.connect(lambda: self.changing_adminoption_stack_PageNo(2))
+        self.viewStats_Btn.clicked.connect(lambda: self.changing_adminStack_PageNo(2))
+        self.addFood_Btn.clicked.connect(lambda: self.changing_adminStack_PageNo(1))
+
+
+        # -------------------- Customer Options ----------------------- #
+        self.explore_Btn.clicked.connect(lambda: self.changing_customerStack_PageNo(1))
+        self.fvt_Btn.clicked.connect(lambda: self.changing_customerStack_PageNo(3))
+        self.cart_Btn.clicked.connect(lambda: self.changing_customerStack_PageNo(4))
+        self.home_Btn.clicked.connect(lambda: self.changing_customerStack_PageNo(0))
 
 
     # ---------------------- Changing Page ------------------------ #
     def changing_mainStack_PageNo(self, index):
         self.mainStack.setCurrentIndex(index)
 
+        if index == 3:
+            self.changing_adminStack_PageNo(0)
+            self.changing_adminoption_stack_PageNo(0)
+        elif index == 4:
+            self.changing_customerStack_PageNo(0)
+
     def changing_adminStack_PageNo(self, index):
         self.adminStack.setCurrentIndex(index)
+
+    def changing_adminoption_stack_PageNo(self, index):
+        self.adminoption_stack.setCurrentIndex(index)
+        self.changing_adminStack_PageNo(0)
 
     def changing_customerStack_PageNo(self, index):
         self.customerStack.setCurrentIndex(index)
